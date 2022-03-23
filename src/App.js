dbcb427a87fc48679785f3ce7a1446a5fe430923
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-03-23 16:42:34 +0700

    adding data.js, edit app.js and index.js, import export

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import dlbia from "./image/dlbia.jpeg";
+import data from "./data.js";
+console.log(data);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={dlbia} className="logo-oasis" />
+        </div>
+        <div>
+          <h3>Don't Look Back In Anger</h3>
+          <h6>Oasis</h6>
+        </div>
+
+        <select name="list" id="list">
+          <option value="musik">Don't Look Back In Anger</option>
+          <option value="musik">Stand by Me</option>
+        </select>
       </header>
     </div>
   );
 }
-
 export default App;
